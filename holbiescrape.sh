58f62e7396342ea2147f5d72548e7f9d97e33111
OctopusHugz <colsonscott53@gmail.com>
2020-12-16 03:52:22 +0000

    Changed holbiescrape.sh to original setup value for peer cloning

--- holbiescrape.sh
+++ holbiescrape.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Run the Holberton project scraper on a link to a Holberton project
 project="$1"
-python2 /home/vagrant/utils/holbiescraper/holbieproject.py "$project"
+python2 ENTER_FULL_PATHNAME_TO_DIRECTORY_HERE/holbieproject.py "$project"
 project_return=$(echo "$?")
 if [[ "$project_return" -ne 1 ]];
-then python2 /home/vagrant/utils/holbiescraper/holbieread.py "$project"
+then python2 ENTER_FULL_PATHNAME_TO_DIRECTORY_HERE/holbieread.py "$project"
 else echo "Looks like there was an error running holbieproject, so I won't run holbieread. Exiting..."
 fi
