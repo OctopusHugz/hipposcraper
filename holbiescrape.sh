d35f27ab112ec8884c8ae036a0e2919d6f7c43b0
OctopusHugz <colsonscott53@gmail.com>
2020-12-16 03:47:50 +0000

    Added logic to holbiescrape that only runs holbieread if holbieproject finishes successfully

--- holbiescrape.sh
+++ holbiescrape.sh
@@ -2,4 +2,8 @@
 # Run the Holberton project scraper on a link to a Holberton project
 project="$1"
 python2 /home/vagrant/utils/holbiescraper/holbieproject.py "$project"
-python2 /home/vagrant/utils/holbiescraper/holbieread.py "$project"
+project_return=$(echo "$?")
+if [[ "$project_return" -ne 1 ]];
+then python2 /home/vagrant/utils/holbiescraper/holbieread.py "$project"
+else echo "Looks like there was an error running holbieproject, so I won't run holbieread. Exiting..."
+fi
