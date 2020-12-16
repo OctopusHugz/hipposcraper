3f2407bff7afee39daf455c00eddfe4c23f1ceb2
OctopusHugz <colsonscott53@gmail.com>
2020-12-16 17:04:57 +0000

    Added install.sh script

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Installs all packages needed to run the Holbiescraper
+sudo apt-get install -y python-pip
+sudo pip install beautifulsoup4==4.6.3
+sudo pip install mechanize==0.3.7
