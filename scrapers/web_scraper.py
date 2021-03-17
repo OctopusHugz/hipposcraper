a8575937998aa59a486d1afbbb840a1df0f2b4ad
OctopusHugz <colsonscott53@gmail.com>
2021-03-17 18:14:31 +0000

    Added updates to web_scraper.py

--- scrapers/web_scraper.py
+++ scrapers/web_scraper.py
@@ -15,11 +15,13 @@ class WebScraper:
         py_flag (int): For write_checker()
         py_js (int): For write_checker()
     """
+    sh_flag = 0
     py_flag = 0
     js_flag = 0
     html_flag = 0
     css_flag = 0
     scss_flag = 0
+    check_flag = 0
 
     def __init__(self, soup):
         self.soup = soup
@@ -46,7 +48,7 @@ class WebScraper:
         """Method to scrape for python file names"""
         return self.soup.find_all(string=re.compile("File: "))
 
-    def html_skeleton(self, file_name):
+    def html_skeleton(file_name):
         html_string = ('<!DOCTYPE html>\n<html lang="en">\n'
                        '<head>\n'
                        '\t<meta charset="UTF-8">\n'
@@ -57,6 +59,38 @@ class WebScraper:
                        '</body>\n</html>\n')
         with open(file_name, "w") as f:
             f.write(html_string)
+
+    def write_file_name(self, file_name, find_pyfile, file_idx):
+        """ Method to write actual file """
+        w_file_name = open(file_name, "w+")
+        if ".py" in file_name:
+            self.py_flag = 1
+            self.check_flag = 1
+            w_file_name.write("#!/usr/bin/env python3\n")
+        elif ".sh" in file_name:
+            self.sh_flag = 1
+            self.check_flag = 1
+            w_file_name.write("#!/bin/bash\n")
+        elif ".js" in file_name:
+            self.js_flag = 1
+            self.check_flag = 1
+            w_file_name.write("#!/usr/bin/node\n")
+        elif ".html" in file_name:
+            self.html_flag = 1
+            self.check_flag = 1
+            self.html_skeleton(file_name)
+        elif ".css" in file_name:
+            self.css_flag = 1
+            self.check_flag = 1
+            w_file_name.write("{\n\t\n}\n")
+        elif ".scss" in file_name:
+            self.scss_flag = 1
+            w_file_name.write("/* My style */\n")
+        # Creating prototypes in parallel with files
+        if find_pyfile != -1:
+            w_file_name.write(self.prototypes_list[file_idx])
+            file_idx += 1
+        w_file_name.close()
 
     def write_files(self):
         """Method to write/create python files
@@ -73,7 +107,7 @@ class WebScraper:
         sys.stdout.write("  -> Creating task files... ")
         for item in self.file_names:
             text_file = item.next_sibling.text
-            if "images" in text_file:
+            if "images/" in text_file:
                 os.mkdir("images")
                 continue
             try:
@@ -97,38 +131,17 @@ class WebScraper:
                 if ", " in text_file:
                     comma_file_names = text_file.split(", ")
                     for file_name in comma_file_names:
-                        make_comma = open(file_name, "w+")
-                        make_comma.close()
+                        self.write_file_name(file_name, find_pyfile, file_idx)
                 elif "." not in text_file and one_dir_check is not 1:
-                    os.mkdir(text_file)
+                    # check if file or dir by checking for digits
+                    contains_digit = any(map(unicode.isdigit, text_file))
+                    if contains_digit:
+                        w_file_name = open(text_file, "w+")
+                        w_file_name.close()
+                    else:
+                        os.mkdir(text_file)
                 else:
-                    w_file_name = open(text_file, "w+")
-                    if ".py" in text_file:
-                        self.py_flag = 1
-                        w_file_name.write("#!/usr/bin/python3\n")
-                    elif ".sh" in text_file:
-                        w_file_name.write("#!/bin/bash\n")
-                    elif ".js" in text_file:
-                        self.js_flag = 1
-                        w_file_name.write("#!/usr/bin/node\n")
-                    elif ".html" in text_file:
-                        self.html_flag = 1
-                        self.html_skeleton(text_file)
-                    elif ".css" in text_file:
-                        self.css_flag = 1
-                        w_file_name.write("{\n\t;\n}")
-                    elif ".scss" in text_file:
-                        self.scss_flag = 1
-                        w_file_name.write("/* My style */\n")
-                    else:
-                        pass
-                    # Creating prototypes in parallel with files
-                    if find_pyfile != -1:
-                        w_file_name.write(self.prototypes_list[file_idx])
-                        file_idx += 1
-                    else:
-                        pass
-                    w_file_name.close()
+                    self.write_file_name(text_file, find_pyfile, file_idx)
             except AttributeError:
                 sys.stdout.write("[ERROR] Failed to create ")
                 sys.stdout.write("task file %s\n" % text_file)
@@ -151,7 +164,8 @@ class WebScraper:
                 dir_file = open(item, "w+")
                 dir_file.close()
             os.chdir("..")
-        self.write_checker()
+        if self.check_flag:
+            self.write_checker()
         print("done")
 
     def write_checker(self):
@@ -159,12 +173,13 @@ class WebScraper:
             f.write("#!/usr/bin/env bash\n")
             if self.js_flag == 1:
                 f.write("semistandard --fix ")
+            elif self.sh_flag == 1:
+                f.write("shellcheck ")
             elif self.py_flag == 1:
                 f.write("pep8 ")
+            # Requires w3c installed and w3 set as alias!
             elif self.html_flag == 1 or self.css_flag == 1:
-                f.write("/home/vagrant/utils/W3C-Validator/w3c_validator.py ")
-            else:
-                print("Doesn't look like check.sh is needed!")
+                f.write("w3 ")
             if self.file_names:
                 for i in self.file_names:
                     f.write('"%s" ' % i.next_sibling.text)
